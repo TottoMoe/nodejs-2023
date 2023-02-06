72e1de42bb26f9aab60fff58027a476c17abb439
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-06 11:57:15 -0800

    add simple api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -35,6 +35,11 @@ const url = require('url');
 
 /////////////////////////////////////
 // SERVER
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -42,9 +47,14 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json'});
+    res.end(data);
+
   } else {
     res.writeHead(404, {
-      'Content-type': 'text/html'
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     });
     res.end('<h1>Page not found!</h1>');
   }
