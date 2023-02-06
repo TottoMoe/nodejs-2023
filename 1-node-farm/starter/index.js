37d0639634056be82da40f71d0aca8d6a754ec2c
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-06 11:11:24 -0800

    second commit

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+
+/////////////////////////////////////
+// FILES
 
 // // Blocking code, synchoronous
 // // Reading file from folder
@@ -10,20 +14,31 @@ const fs = require('fs');
 // console.log('File written!');
 
 // Non-blocking, asynchronous
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!💥');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!💥');
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, "utf-8", err => {
-        console.log("Your file has been written 😄");
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, "utf-8", err => {
+//         console.log("Your file has been written 😄");
+//       });
+//     });
+//   });
 
   
+// });
+// console.log('will read file!')
+
+/////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.end('Hello from the server!');
 });
-console.log('will read file!')
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+})
